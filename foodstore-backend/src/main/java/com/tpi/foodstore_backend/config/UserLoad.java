d6017840eaa893579f73489d0accc3742405bd69
Matias <vazquez.matias.e@gmail.com>
2026-06-16 12:42:46 -0300

    feat: Infraestructura base backend completa

---
+++ foodstore-backend/src/main/java/com/tpi/foodstore_backend/config/UserLoad.java
@@ -0,0 +1,30 @@
+package com.tpi.foodstore_backend.config;
+
+import com.tpi.foodstore_backend.model.Usuario;
+import com.tpi.foodstore_backend.model.enums.Rol;
+import com.tpi.foodstore_backend.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserLoad implements CommandLineRunner {
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String[] args) {
+        if (usuarioRepository.count() == 0) {
+            Usuario admin = Usuario.builder()
+                    .nombre("Matias")
+                    .apellido("Vazquez")
+                    .email("vazquez.matias.e@gmail.com")
+                    .password(passwordEncoder.encode("123456"))
+                    .rol(Rol.ADMIN)
+                    .build();
+
+            usuarioRepository.save(admin);
+        }
+    }
+}
